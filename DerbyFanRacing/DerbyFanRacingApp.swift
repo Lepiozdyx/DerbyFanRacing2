eae7cf45ebc5ef10dc689b434e59b5ad493b457c
Developer <Lepiozdyx@gmail.com>
2026-02-16 10:50:23 -0300

    Initial Commit

---
+++ DerbyFanRacing/DerbyFanRacingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DerbyFanRacingApp.swift
+//  DerbyFanRacing
+//
+//  Created by Alex on 16.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct DerbyFanRacingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
